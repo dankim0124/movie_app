f2d8eb864abeab5c3d80451fe299e2f6422b3d53
dohyun <dankim0124@gmail.com>
2020-08-03 12:52:25 +0900

    Updates

---
+++ precache-manifest.ed3e025a4c7c473d9d2045826a973a63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68645af94ade7ad39cd27025e251d792",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "cfef6c3a40e5bdd128f6",
+    "url": "/movie_app/static/css/main.7df52df9.chunk.css"
+  },
+  {
+    "revision": "668dd21e8c648d577a3e",
+    "url": "/movie_app/static/js/2.dc66dd34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.dc66dd34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfef6c3a40e5bdd128f6",
+    "url": "/movie_app/static/js/main.2301d954.chunk.js"
+  },
+  {
+    "revision": "b86e67b41078e2a296f2",
+    "url": "/movie_app/static/js/runtime-main.38b55cde.js"
+  }
+]);
